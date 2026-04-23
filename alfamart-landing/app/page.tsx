1a9072beb265aede3ab7af4d88840e492d4f8ebd
Nirmala <nirmalawilujeng@gmail.com>
2026-04-23 21:49:29 +0700

    client fix

--- alfamart-landing/app/page.tsx
+++ alfamart-landing/app/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+
 import SliderPromo from "@/components/SliderPromo";
 import React from "react";
 import { Phone, MessageCircle, Mail } from "lucide-react";
