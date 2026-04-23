bf34aaca8cf737aceee394afbd88f0aec2b923f6
Nirmala <nirmalawilujeng@gmail.com>
2026-04-23 21:46:02 +0700

    fix again

--- alfamart-landing/next.config.ts
+++ alfamart-landing/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/CS9-SBD",
-  assetPrefix: "/CS9-SBD",
 };
 
 export default nextConfig;
